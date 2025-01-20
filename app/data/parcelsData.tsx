bfc2f3c46c17464b530073f42139397b70224b49
David Pechtalt <davidpechtalt@gmail.com>
2025-01-20 12:23:09 +0200

    edit parcel

--- app/data/parcelsData.tsx
+++ app/data/parcelsData.tsx
@@ -21,3 +21,17 @@ export function pickParcel(parcelId:string){
 export function findParcel(parcelId:string){
   return parcels.find(parcel => parcel.id === parcelId)
 }
+
+
+export function editParcel(parcel:Omit<Parcel,'status'|'arrivedIn'>){
+  const parcelIndex = parcels.findIndex( p=> p.id === parcel.id);
+  if(parcelIndex === -1){
+    return false;
+  }
+  const upadatedParcel:Parcel ={
+    ...parcel,
+    status:parcels[parcelIndex].status,
+    arrivedIn:parcels[parcelIndex].arrivedIn
+  }
+  parcels[parcelIndex] = upadatedParcel
+}
--- app/routes/parcels.$parcelId.edit.tsx
+++ app/routes/parcels.$parcelId.edit.tsx
@@ -1,7 +1,60 @@
-import { LoaderFunctionArgs } from "@remix-run/node";
-import { useLoaderData } from "@remix-run/react";
+import { ActionFunctionArgs, LoaderFunctionArgs } from "@remix-run/node";
+import { redirect, useLoaderData } from "@remix-run/react";
 import ParcelForm from "~/components/parcelForm";
-import { findParcel } from "~/data/parcelsData";
+import {  editParcel, findParcel } from "~/data/parcelsData";
+import { getResidentByName, residentsNames } from "~/data/residentsData";
+import { Parcel } from "~/types/parcel";
+import { isParcelCourier } from "~/types/parcelCourier";
+import { ParcelFormError } from "~/types/parcelFormError";
+import { isParcelLocation } from "~/types/parcelLocation";
+
+export async function action({ request }: ActionFunctionArgs) {
+  const formData = await request.formData();
+  const id = formData.get("id");
+  const courier = formData.get("courier");
+  const location = formData.get("location");
+  const note = formData.get("note");
+  const residentName = formData.get("resident");
+  let error: ParcelFormError | undefined
+  if (
+    typeof id !== "string" ||
+    typeof courier !== "string" ||
+    typeof location !== "string" ||
+    typeof note !== "string" ||
+    typeof residentName !== "string"
+  ) {
+    error="input error"
+    return Response.json(
+      {
+        error
+      },
+      { status: 400 }
+    );
+  }
+  
+  if (!isParcelCourier(courier)) {
+    error = "courier error";
+  } else if (!isParcelLocation(location)) {
+    error = "location error";
+  } else if (!residentsNames.includes(residentName)) {
+    error = "resident error";
+  }
+  if (error) return Response.json({ error }, { status: 400 });
+  const resident = getResidentByName(residentName);
+  if(!resident){
+    return Response.json({error}, {status:400})
+  }
+  const upadatedParcel = {
+    id,
+    courier,
+    location,
+    note,
+   
+    resident,
+  };
+  editParcel(upadatedParcel as Omit<Parcel,'status'|'arrivedIn'>)
+  return redirect("..");
+}
 
 export function loader({params}:LoaderFunctionArgs){
     const parcelId = params.parcelId ;
