77e7c33afabf1b44e0a96d1e230a98651630306c
David Pechtalt <davidpechtalt@gmail.com>
2025-02-04 14:48:45 +0200

    end date in search params

--- app/routes/parcels.tsx
+++ app/routes/parcels.tsx
@@ -55,6 +55,7 @@ export default function Parcels() {
     !formData.get("status") && formData.delete("status");
     //the property is the third value when choosing end day
     !formData.get("property") && formData.delete("property");
+    !formData.get("end-date")&& formData.delete("end-date")
     submit(formData);
   }
 
